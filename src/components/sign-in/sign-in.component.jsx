cddaa129a3c4e1db9e185aebb026e9002b5034d3
sidmal11 <39958179+sidmal11@users.noreply.github.com>
2021-02-10 15:20:45 -0500

    finishing sign in handleSubmit method

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -4,7 +4,7 @@ import "./sign-in.styles.scss";
 import FormInput from "../form-input/form-input.component";
 import CustomButton from "../custom-button/custom-button.component";
 
-import { signInWithGoogle } from "../../firebase/firebases.utils";
+import { auth, signInWithGoogle } from "../../firebase/firebases.utils";
 
 class SignIn extends Component {
   constructor(props) {
@@ -16,9 +16,17 @@ class SignIn extends Component {
     };
   }
 
-  handleSubmit = (event) => {
+  handleSubmit = async (event) => {
     event.preventDefault();
-    this.setState({ email: "", password: "" });
+
+    const { email, password } = this.state;
+
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
+      this.setState({ email: "", password: "" });
+    } catch (err) {
+      console.error(err);
+    }
   };
 
   handleChange = (event) => {
