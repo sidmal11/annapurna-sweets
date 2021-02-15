54b315428b7deb1287ae923826ffc8ffc5de87a4
sidmal11 <39958179+sidmal11@users.noreply.github.com>
2021-02-15 13:16:23 -0500

    updated cart utils to group items in cart reducer

---
+++ src/redux/cart/cart.utils.js
@@ -0,0 +1,16 @@
+export const addItemToCart = (cartItems, cartItemToAdd) => {
+  const existingCartItem = cartItems.find(
+    (cartItem) => cartItem.id === cartItemToAdd.id
+  );
+
+  if (existingCartItem) {
+    return cartItems.map((cartItem) =>
+      cartItem.id === cartItemToAdd.id
+        ? { ...cartItem, quantity: cartItem.quantity + 1 }
+        : cartItem
+    );
+  }
+
+  //if exisiting item is not found add it to the list with base quantity as one
+  return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
+};
