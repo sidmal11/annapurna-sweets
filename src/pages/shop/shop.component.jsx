a481bbca13626e88abc30872d70b5e7a121dd40d
sidmal11 <39958179+sidmal11@users.noreply.github.com>
2021-02-18 15:04:06 -0500

    advanced routing and making use of match.params

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+import { Route } from "react-router-dom";
 
 import CollectionOverview from "../../components/collections-overview/collections-overview.component";
+import CategoryPage from "../category/category.component";
 
-const ShopPage = ({ collections }) => (
-  <div className="shop=page">
-    <CollectionOverview />
+const ShopPage = ({ match }) => (
+  <div className="shop-page">
+    <Route exact path={`${match.path}`} component={CollectionOverview} />
+    <Route path={`${match.path}/:categoryId`} component={CategoryPage} />
   </div>
 );
 
