d6b15cf606ec30cfb6e1d333a71dcadebeebae72
sidmal11 <39958179+sidmal11@users.noreply.github.com>
2021-02-18 11:43:29 -0500

    restructured the project by assigning a layer of collectionsoverview in between for exapnding a specific category

---
+++ src/pages/shop/shop.component.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import CollectionOverview from "../../components/collections-overview/collections-overview.component";
+
+const ShopPage = ({ collections }) => (
+  <div className="shop=page">
+    <CollectionOverview />
+  </div>
+);
+
+export default ShopPage;
